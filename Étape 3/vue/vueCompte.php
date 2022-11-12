9d8631ff35f848596eb995d0ce410e1da580e7e7
Mael Mane <mmane@dept-info.crosemont.quebec>
2022-11-12 21:17:55 +0000

    Replace vueCompte.php

--- Étape 3/vue/vueCompte.php
+++ Étape 3/vue/vueCompte.php
@@ -1,8 +1,8 @@
 <!--
     Auteur: Mael Mane
     Date de créaton: 19/10/2022
-    Dernière modifcation: 10/11/2022
-    Modifié par: Lesly Gourdet
+    Dernière modifcation: 12/11/2022
+    Modifié par: Mael Mane
 -->
 
 <!DOCTYPE html>
@@ -15,14 +15,14 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
         <link href="css/styleCompte.css" rel="stylesheet">
-        <title>Votre Compte</title>
+        <title>ParatgeDeDocuments</title>
     </head>
     <body>
         <header>
             <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
               <div class="container-fluid">
                     <a class="navbar-brand" href="vueAcceuil.html">
-                        <img id="logo" src="img/logoOrange.png" alt="Logo Partage De Documents"/>
+                        <img id="logo" src="img/logoOrange.png" alt="Logo ParatgeDeDocuments"/>
                     </a>
         
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar"> <!--Pour écran mobile faire menu défilant -->
@@ -44,7 +44,7 @@
 
         <div class="container mt-3">
             <!--<div class="mt-4 p-5 text-white text-center orangeBg">
-                <h1>Informations du compte</h1>
+                <h1>Inforamtions du compte</h1>
                 <p>Bonjour, "user" voici les informations de votre compte</p>	
             </div>-->
             
@@ -54,6 +54,7 @@
                         <h2>Vos Documents</h2>
                         <button class="btn btnOrange" type="button">Ajouter un document <i class="bi bi-cloud-arrow-up"></i></button>
                         <div class="docs">
+                            
                             <div class="card">  <!--Une card pour chaque document...??-->
                                 <div class="card-body">
                                     <p>NomDocument</p>
@@ -68,37 +69,36 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="card">  <!--Une card pour chaque document...??-->
-                                <div class="card-body">
-                                    <p>NomDocument</p>
-                                    <a class="hoverName" href="#">NomUser</a> <!--Faudrait link vers leur profil si possible-->
-                                    <div class="dropdown">
-                                        <button type="button" class="btn btnOrange dropdown-toggle" data-bs-toggle="dropdown">Visibilité</button>
-                                        <ul class="dropdown-menu">
-                                          <li><a class="dropdown-item" href="#">Public</a></li>
-                                          <li><a class="dropdown-item" href="#">Privé</a></li>
-                                          <li><a class="dropdown-item" href="#">Protégé</a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="card">  <!--Une card pour chaque document...??-->
-                                <div class="card-body">
-                                    <p>NomDocument</p>
-                                    <a class="hoverName" href="#">NomUser</a> <!--Faudrait link vers leur profil si possible-->
-                                    <div class="dropdown">
-                                        <button type="button" class="btn btnOrange dropdown-toggle" data-bs-toggle="dropdown">Visibilité</button>
-                                        <ul class="dropdown-menu">
-                                          <li><a class="dropdown-item" href="#">Public</a></li>
-                                          <li><a class="dropdown-item" href="#">Privé</a></li>
-                                          <li><a class="dropdown-item" href="#">Protégé</a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
+                            
+                            <?php
+                            try{
+                                //Établir une connexion avec la base de données partagedoc
+                                $cnx = new PDO('mysql:host=localhost; dbname=partagedoc', "root", "root");
+                                
+                                //Prendre les données des input du form  dans vueCreerCompte
+                                
+                                //Insérer les données dans la table compte et l'executer
+                                $requette = "SELECT * FROM documents WHERE  username = 'user1'";        //Exemple avec user1 ... faudrait avec username de la session?
+                                $resultat = $cnx->query($requette);
+
+                                foreach ($resultat as $row){
+                                    echo "<div class='card'>";
+                                        echo "<div class='card-body'>";
+                                            echo "<p>".$row["titre"]."</p>";
+                                            echo "<a class='hoverName'>".$row["username"]."</a>";
+                                        echo "</div>";
+                                    echo "</div>";
+                                }
+                                $resultat->closeCursor();
+                            } catch (PDOException $e){
+                            print "Erreur!: " . $e->getMessage() . "<br/>";
+                            die();
+                            } finally {
+                                //Fermer la connexion avec la base de données
+                                $cnx=null;
+                            }
+                        ?>
                         </div>
-                        <button class="btn btnOrange" type="button">Télécharger un document <i class="bi bi-cloud-arrow-down"></i></button>
-                        <button class="btn btnOrange" type="button">Supprimer un document <i class="bi bi-cloud-minus"></i></button>
                     </article>
                 </main>
                 <aside class="col-6 col-md-3">
