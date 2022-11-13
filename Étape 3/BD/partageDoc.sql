6db44aa2fedd5428a48705634d52a25ff5daccc5
Mael Mane <mmane@dept-info.crosemont.quebec>
2022-11-13 01:54:43 +0000

    Replace partageDoc.sql

--- Étape 3/BD/partageDoc.sql
+++ Étape 3/BD/partageDoc.sql
@@ -2,12 +2,14 @@
 -- Host:                         127.0.0.1
 -- Server version:               5.7.11 - MySQL Community Server (GPL)
 -- Server OS:                    Win32
--- HeidiSQL Version:             11.3.0.6295
+-- HeidiSQL Version:             12.1.0.6537
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
@@ -16,22 +18,6 @@
 -- Dumping database structure for partagedoc
 CREATE DATABASE IF NOT EXISTS `partagedoc` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `partagedoc`;
-
--- Dumping structure for table partagedoc.compte
-CREATE TABLE IF NOT EXISTS `compte` (
-  `username` varchar(100) NOT NULL DEFAULT '',
-  `email` varchar(100) NOT NULL DEFAULT '',
-  `passwd` varchar(100) NOT NULL DEFAULT '',
-  PRIMARY KEY (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Dumping data for table partagedoc.compte: ~3 rows (approximately)
-/*!40000 ALTER TABLE `compte` DISABLE KEYS */;
-INSERT INTO `compte` (`username`, `email`, `passwd`) VALUES
-	('test', 'test@example.com', '12345'),
-	('test2', 'test2@example.com', '4321'),
-	('user1', 'user1@example.com', '1234');
-/*!40000 ALTER TABLE `compte` ENABLE KEYS */;
 
 -- Dumping structure for table partagedoc.documents
 CREATE TABLE IF NOT EXISTS `documents` (
@@ -46,12 +32,10 @@ CREATE TABLE IF NOT EXISTS `documents` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Dumping data for table partagedoc.documents: ~3 rows (approximately)
-/*!40000 ALTER TABLE `documents` DISABLE KEYS */;
 INSERT INTO `documents` (`idDoc`, `titre`, `username`, `nbLike`, `statut`) VALUES
 	(1, 'Exemple1.html', 'user1', 21, 'public'),
 	(2, 'Exemple3.php', 'test2', 45, 'public'),
 	(3, 'Devoir4.py', 'user1', 2, 'public');
-/*!40000 ALTER TABLE `documents` ENABLE KEYS */;
 
 -- Dumping structure for table partagedoc.relation
 CREATE TABLE IF NOT EXISTS `relation` (
@@ -62,9 +46,18 @@ CREATE TABLE IF NOT EXISTS `relation` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Dumping data for table partagedoc.relation: ~0 rows (approximately)
-/*!40000 ALTER TABLE `relation` DISABLE KEYS */;
-/*!40000 ALTER TABLE `relation` ENABLE KEYS */;
 
+-- Dumping structure for table partagedoc.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table partagedoc.users: ~0 rows (approximately)
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
