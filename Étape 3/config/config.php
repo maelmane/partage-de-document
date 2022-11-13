79597babc2aee355e1efa7417cd5994ad4078030
Mael Mane <mmane@dept-info.crosemont.quebec>
2022-11-13 17:52:55 +0000

    Replace config.php

--- Étape 3/config/config.php
+++ Étape 3/config/config.php
@@ -13,8 +13,8 @@
     define('DB_NAME','partagedoc');
 
     try{
-        $pdo = new PDO("mysql:host=" .DB_SERVER. ";dbname=" .DB_NAME, DB_USERNAME, DB_PASSWORD);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $cnx = new PDO("mysql:host=" .DB_SERVER. ";dbname=" .DB_NAME, DB_USERNAME, DB_PASSWORD);
+        $cnx->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch(PDOException $e){
         die("ERREUR: " . $e->getMessage());
     }
