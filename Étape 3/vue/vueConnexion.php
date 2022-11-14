237920f025a310aa4fb14d784bc49942d35f58ac
Mael Mane <mmane@dept-info.crosemont.quebec>
2022-11-14 14:58:07 +0000

    Replace vueConnexion.php

--- Étape 3/vue/vueConnexion.php
+++ Étape 3/vue/vueConnexion.php
@@ -1,7 +1,7 @@
 <!--
     Auteur: Mael Mane
     Date de créaton: 19/10/2022
-    Dernière modifcation: 12/11/2022
+    Dernière modifcation: 14/11/2022
     Modifié par: Mael Mane
 -->
 
@@ -11,7 +11,7 @@ session_start();
  
 // Vérifier si l'utilisateur est déja logged in, si oui le rediriger vers la page d'acceuil
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: vueAcceuil.html");
+    header("location: vueAcceuil.php");
     exit;
 }
  
@@ -102,7 +102,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/styleCreerCompte.css">
+    <link rel="stylesheet" href="/css/styleConnexion.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
