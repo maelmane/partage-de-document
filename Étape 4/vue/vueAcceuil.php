3ef5b3ff40a219f0cd1ddccc33c0ae5f7e5b360e
Mael Mane <mmane@dept-info.crosemont.quebec>
2022-12-15 00:30:03 +0000

    Replace vueAcceuil.php

--- Étape 4/vue/vueAcceuil.php
+++ Étape 4/vue/vueAcceuil.php
@@ -13,7 +13,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
-        <link href="../css/styleAcceuil.css" rel="stylesheet">
+        <link href="css/styleAcceuil.css" rel="stylesheet">
         <title>PartageDeDocuments</title>
   </head>
   <body>
