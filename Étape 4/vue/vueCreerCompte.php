6dc7c3b7ac52e98b121242a1b3607c215457d1a0
Mael Mane <mmane@dept-info.crosemont.quebec>
2022-12-15 00:29:44 +0000

    Replace vueCreerCompte.php

--- Étape 4/vue/vueCreerCompte.php
+++ Étape 4/vue/vueCreerCompte.php
@@ -1,7 +1,7 @@
 <!--
     Auteur: Mael Mane
     Date de créaton: 19/10/2022
-    Dernière modifcation: 12/11/2022
+    Dernière modifcation: 14/12/2022
     Modifié par: Mael Mane
 -->
 
@@ -99,7 +99,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/styleCreerCompte.css">
+    <link rel="stylesheet" href="css/styleCreerCompte.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
