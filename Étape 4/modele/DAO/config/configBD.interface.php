777a613ca7765d61367ccdd956121257be17eaa3
Mael Mane <mmane@dept-info.crosemont.quebec>
2022-12-17 21:53:32 +0000

    Replace configBD.interface.php

--- Étape 4/modele/DAO/config/configBD.interface.php
+++ Étape 4/modele/DAO/config/configBD.interface.php
@@ -1,8 +1,8 @@
 <!--
     Auteur: Mael Mane
     Date de créaton: 16/11/2022
-    Dernière modifcation: 15/12/2022
-	Modifié par : Lesly Gourdet
+    Dernière modifcation: 17/12/2022
+	Modifié par : Mael Mane
 -->
 <?php	
 	interface ConfigBD
@@ -10,7 +10,7 @@
 		//const URL = "localhost/partageDoc";
 		const DB_HOST = "localhost";
 		const DB_USER = "root";
-		const DB_PWD = "";
+		const DB_PWD = "root";
 		const DB_NAME = "application";
 	}
 ?>
