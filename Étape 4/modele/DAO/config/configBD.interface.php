6b2b877cf27378df3b49423606e8d51f4fb39665
Lesly-Junior Gourdet <lgourdet@dti.crosemont.quebec>
2022-12-19 08:35:35 -0500

    config

--- Étape 4/modele/DAO/config/configBD.interface.php
+++ Étape 4/modele/DAO/config/configBD.interface.php
@@ -9,7 +9,7 @@
 		//const URL = "localhost/application";
 		const DB_HOST = "localhost";
 		const DB_USER = "root";
-		const DB_PWD = "";
+		const DB_PWD = "root";
 		const DB_NAME = "application";
 	}
 ?>
